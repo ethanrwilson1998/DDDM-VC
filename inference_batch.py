04d7910437ef827054e566416ec4e6baef1687b0
Ethan Wilson <ethanrwilson1998@gmail.com>
2025-04-02 21:15:50 -0400

    batch references method to create new dataset files

--- inference_batch.py
+++ inference_batch.py
@@ -1,157 +1,61 @@
-import ntpath
-import os
-import torch
 import argparse
-import json
-from glob import glob
+import glob
+import os
+
 import tqdm
-import numpy as np
-from torch.nn import functional as F
-import commons
-from scipy.io.wavfile import write
-import torchaudio
-import utils
-from data_loader import MelSpectrogramFixed
 
-import amfm_decompy.basic_tools as basic
-import amfm_decompy.pYAAPT as pYAAPT
-from model.vc_dddm_mixup import SynthesizerTrn, Wav2vec2, DDDM
-from vocoder.hifigan import HiFi
-from model_f0_vqvae import Quantizer
-
-h = None
-device = None
-seed = 1234
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)  
-
-def load_audio(path):
-    audio, sr = torchaudio.load(path) 
-    audio = audio[:1]
-    if sr != 16000:
-        audio = torchaudio.functional.resample(audio, sr, 16000, resampling_method="kaiser_window")
-    
-    p = (audio.shape[-1] // 1280 + 1) * 1280 - audio.shape[-1] 
-    audio = torch.nn.functional.pad(audio, (0, p)) 
-     
-    return audio 
+from inference import inference
 
 
-def save_audio(wav, out_file, syn_sr=16000):
-    wav = (wav.squeeze() / wav.abs().max() * 0.999 * 32767.0).cpu().numpy().astype('int16')
-    write(out_file, syn_sr, wav) 
-        
+def process_folder(a):
+    audio_clips = glob.glob(f"{a.audio_folder}/**/*.wav", recursive=True)
 
-def get_yaapt_f0(audio, sr=16000, interp=False):
-    to_pad = int(20.0 / 1000 * sr) // 2
-    f0s = []
-    for y in audio.astype(np.float64):
-        y_pad = np.pad(y.squeeze(), (to_pad, to_pad), "constant", constant_values=0) 
-        pitch = pYAAPT.yaapt(basic.SignalObj(y_pad, sr), 
-                             **{'frame_length': 20.0, 'frame_space': 5.0, 'nccf_thresh1': 0.25, 'tda_frame_length': 25.0})
-        f0s.append(pitch.samp_interp[None, None, :] if interp else pitch.samp_values[None, None, :])
+    for clip in tqdm.tqdm(audio_clips):
+        folders = clip.split(os.sep)
+        dataset_name = folders[-4]
+        if a.method == "VoiceVMF":
+            anon_dataset_name = f"{dataset_name}_VoiceVMF_e{a.epsilon}"
 
-    return np.vstack(f0s)
+        new_clip = clip.replace(dataset_name, anon_dataset_name)
 
+        print(f"Processing {clip}...")
+        a.src_path = clip
+        if a.method in ["VoiceVMF"]:
+            a.trg_path = (
+                clip  # Since we are processing per participant, keep trg_path same
+            )
+        a.output_path = new_clip
+        inference(a)
 
-def inference(a): 
-    mel_fn = MelSpectrogramFixed(
-        sample_rate=hps.data.sampling_rate,
-        n_fft=hps.data.filter_length,
-        win_length=hps.data.win_length,
-        hop_length=hps.data.hop_length,
-        f_min=hps.data.mel_fmin,
-        f_max=hps.data.mel_fmax,
-        n_mels=hps.data.n_mel_channels,
-        window_fn=torch.hann_window
-    ).cuda()
-
-    # Load pre-trained w2v (XLS-R)
-    w2v = Wav2vec2().cuda()  
-        
-    # Load model
-    f0_quantizer = Quantizer(hps).cuda()
-    utils.load_checkpoint(a.ckpt_f0_vqvae, f0_quantizer)
-    f0_quantizer.eval()
-
-    model = DDDM(hps.data.n_mel_channels, hps.diffusion.spk_dim,
-                   hps.diffusion.dec_dim, hps.diffusion.beta_min, hps.diffusion.beta_max, hps).cuda()
-    utils.load_checkpoint(a.ckpt_model, model, None)
-    model.eval()
- 
-    # Load vocoder
-    net_v = HiFi(hps.data.n_mel_channels, hps.train.segment_size // hps.data.hop_length, **hps.model).cuda()
-    utils.load_checkpoint(a.ckpt_voc, net_v, None)
-    net_v.eval().dec.remove_weight_norm()   
- 
-    # Convert audio 
-    print(f'>> Processing {a.src_path}...') 
-    audio = load_audio(a.src_path)   
-
-    src_mel = mel_fn(audio.cuda())
-    src_length = torch.LongTensor([src_mel.size(-1)]).cuda()
-    w2v_x = w2v(F.pad(audio, (40, 40), "reflect").cuda())
-
-    try:
-        f0 = get_yaapt_f0(audio.numpy())
-    except:
-        f0 = np.zeros((1, audio.shape[-1] // 80), dtype=np.float32) 
- 
-    ii = f0 != 0
-    f0[ii] = (f0[ii] - f0[ii].mean()) / f0[ii].std() 
-    f0 = torch.FloatTensor(f0).cuda()
-    f0_code = f0_quantizer.code_extraction(f0)
-
-    with torch.no_grad(): 
-        c = model.vc(src_mel, w2v_x, f0_code, src_length, src_mel, src_length, n_timesteps=a.time_step, mode='ml', eps=a.epsilon, theta=a.theta)
-        converted_audio = net_v(c)  
-
-    save_audio(converted_audio, a.output_path)   
-    print(f"Processed and saved: {a.output_path}")
-
-
-def process_folder(audio_folder):
-    participants = sorted(os.listdir(audio_folder))  # Get sorted participant folders
-    
-    for participant in participants:
-        participant_path = os.path.join(audio_folder, participant)
-        
-        if not os.path.isdir(participant_path):
-            continue  # Skip if not a directory
-        
-        audio_clips = sorted(glob(os.path.join(participant_path, "*.wav")))
-        
-        for clip in audio_clips:
-            print(f'Processing {clip}...')
-            a.src_path = clip
-            a.trg_path = clip  # Since we are processing per participant, keep trg_path same
-            a.output_path = clip  # Overwrite the original file
-            inference(a)
-            
     print(">> Processing Complete.")
 
 
 def main():
-    print('>> Initializing Inference Process...')
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--audio_folder', type=str, default = './voxceleb', help='Path to folder containing participant audio')
-    parser.add_argument('--ckpt_model', type=str, default='./ckpt/model_base.pth')
-    parser.add_argument('--ckpt_voc', type=str, default='./vocoder/voc_ckpt.pth')
-    parser.add_argument('--ckpt_f0_vqvae', '-f', type=str, default='./f0_vqvae/f0_vqvae.pth')
-    parser.add_argument('--time_step', '-t', type=int, default=6)
-    parser.add_argument('--epsilon', type=float, default=1000)
-    parser.add_argument('--theta', type=float, default=0)
-    
-    global hps, device, a
-    
-    a = parser.parse_args()
-    config = os.path.join(os.path.split(a.ckpt_model)[0], 'config.json')  
-    hps = utils.get_hparams_from_file(config) 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    process_folder(a.audio_folder)
-    
+    print(">> Initializing Inference Process...")
 
-if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--audio_folder",
+        type=str,
+        default="./voxceleb",
+        help="Path to folder containing participant audio",
+    )
+    parser.add_argument("--ckpt_model", type=str, default="./ckpt/model_base.pth")
+    parser.add_argument("--ckpt_voc", type=str, default="./vocoder/voc_ckpt.pth")
+    parser.add_argument(
+        "--ckpt_f0_vqvae", "-f", type=str, default="./f0_vqvae/f0_vqvae.pth"
+    )
+    parser.add_argument("--time_step", "-t", type=int, default=6)
+    parser.add_argument(
+        "--method", choices=["swap", "VoiceVMF", "IdentityDP"], type=str, default="swap"
+    )
+    parser.add_argument("--epsilon", type=float, default=1000)
+    parser.add_argument("--theta", type=float, default=0)
+
+    a = parser.parse_args()
+
+    process_folder(a)
+
+
+if __name__ == "__main__":
     main()
